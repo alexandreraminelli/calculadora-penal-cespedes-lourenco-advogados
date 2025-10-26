9828a2a6b59e725f2ad50f9b86f96345ce835dea
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-26 16:14:20 -0300

    feat(logo): exibir logo em cima do form

--- CalculadoraPenal/app/src/main/java/br/com/cespedeslourencoadvogados/calculadorapenal/ui/features/calculator/CalculatorScreen.kt
+++ CalculadoraPenal/app/src/main/java/br/com/cespedeslourencoadvogados/calculadorapenal/ui/features/calculator/CalculatorScreen.kt
@@ -1,26 +1,39 @@
 package br.com.cespedeslourencoadvogados.calculadorapenal.ui.features.calculator
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.cespedeslourencoadvogados.calculadorapenal.R
 
 /** Formulário da Calculadora Penal. */
 @Composable
 fun CalculatorScreen(
     modifier: Modifier = Modifier
 ) {
-    Column (
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 20.dp)
     ) {
-        Text(text = "Calculadora Penal")
+        // Imagem da logo
+        Image(
+            painter = painterResource(R.drawable.logo_cor),
+            contentDescription = null,
+        )
     }
 }
 
 
 /** Prévia do formulário da calculadora penal. */
-@Preview
+@Preview(showBackground = true)
 @Composable
 fun CalculatorScreenPreview() {
     CalculatorScreen()
