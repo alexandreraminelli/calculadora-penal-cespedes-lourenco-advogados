b01e3a3fd029431b8eddf311f38cadb304784876
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-10-26 19:05:51 -0300

    feat(form): botão de calcular pena

--- CalculadoraPenal/app/src/main/java/br/com/cespedeslourencoadvogados/calculadorapenal/ui/features/calculator/CalculatorScreen.kt
+++ CalculadoraPenal/app/src/main/java/br/com/cespedeslourencoadvogados/calculadorapenal/ui/features/calculator/CalculatorScreen.kt
@@ -3,12 +3,21 @@ package br.com.cespedeslourencoadvogados.calculadorapenal.ui.features.calculator
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.cespedeslourencoadvogados.calculadorapenal.R
@@ -53,6 +62,17 @@ fun CalculatorScreen(
             label = R.string.label_status_apenado,
             options = StatusApenado.values().toList()
         )
+
+        // Botão de enviar
+        Spacer(modifier = Modifier)
+        Button(
+            onClick = {
+                // TODO: calcular pena e ir pra próxima tela
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.calcular))
+        }
     }
 }
 
