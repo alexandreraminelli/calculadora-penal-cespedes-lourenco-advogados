a842a45ff467d3ba375b7ade643313044a6a59ba
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2025-11-01 18:13:30 -0300

    feat(model): adicionar serialização ao `PenaltyDuration`

--- CalculadoraPenal/app/src/main/java/br/com/cespedeslourencoadvogados/calculadorapenal/data/model/PenaltyDuration.kt
+++ CalculadoraPenal/app/src/main/java/br/com/cespedeslourencoadvogados/calculadorapenal/data/model/PenaltyDuration.kt
@@ -1,6 +1,9 @@
 package br.com.cespedeslourencoadvogados.calculadorapenal.data.model
 
+import kotlinx.serialization.Serializable
+
 /** Modelo de dados para durações de tempo em anos, meses e dias. */
+@Serializable
 data class PenaltyDuration(
     val years: Int = 0,
     val months: Int = 0,
